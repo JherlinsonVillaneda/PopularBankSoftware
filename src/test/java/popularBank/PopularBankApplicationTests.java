2f16f4245e689ae4cb223976cd1db3b83ff411d8
Jherlinson Stiven Villaneda Carvajal <jherlinson.villaneda@globant.com>
2024-01-15 14:34:16 -0500

    First Commit

---
+++ src/test/java/popularBank/PopularBankApplicationTests.java
@@ -0,0 +1,13 @@
+package popularBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopularBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
